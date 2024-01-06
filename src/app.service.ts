8370eb24d88f269f7f21a2b7c2ad16f1675803af
Petr Molchanov <p.molchanov@rogii.com>
2024-01-06 19:56:28 -0300

    add Dockerfile

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  constructor() {
+    console.log(process.env.MONGO_URI);
+  }
   getHello(): string {
     return 'Hello World!';
   }
