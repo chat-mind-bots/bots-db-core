4bd67bfc27236712c2436cdc6e1e73fca71c411f
Petr Molchanov <p.molchanov@rogii.com>
2024-01-07 21:55:42 -0300

    update collect user logic

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,17 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { UserService } from './user.service';
-import { CREATE_USER } from '@chat-mind-bots/rabbit-patterns';
-import { CreateUserDto } from '@chat-mind-bots/bots-models';
+import {
+  CREATE_USER,
+  ICreateUserPayload,
+} from '@chat-mind-bots/rabbit-patterns';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @MessagePattern(CREATE_USER)
-  create(@Payload() createUserDto: CreateUserDto) {
+  create(@Payload() createUserDto: ICreateUserPayload) {
     return this.userService.create(createUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto, User, UserDocument } from '@chat-mind-bots/bots-models';
+import { User, UserDocument } from '@chat-mind-bots/bots-models';
+import { ICreateUserPayload } from '@chat-mind-bots/rabbit-patterns';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -9,21 +10,30 @@ export class UserService {
     @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
   ) {}
 
-  async create(createUserDto: CreateUserDto) {
+  async create(createUserDto: ICreateUserPayload) {
+    const { telegram, botLogin } = createUserDto;
+
     let userInDB = false;
-    if (createUserDto.telegram?.id) {
-      const oldUser = await this.userModel
-        .findOne({ 'telegram.id': createUserDto.telegram?.id })
+
+    if (telegram?.id) {
+      const oldUser: null | UserDocument = await this.userModel
+        .findOne({ 'telegram.id': telegram?.id })
         .exec();
+
       if (oldUser) {
         userInDB = true;
+
+        if (!oldUser?.bots.includes(botLogin)) {
+          await oldUser.updateOne({ bots: [...oldUser.bots, botLogin] });
+        }
       }
     }
     if (userInDB) {
       return;
     }
     const user = await this.userModel.create({
-      telegram: createUserDto.telegram,
+      telegram: telegram,
+      bots: [botLogin],
     });
     return user;
   }
